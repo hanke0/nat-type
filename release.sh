057e2047878cba4cc55630f5914c05b237eaf1ea
hanke <hanke0@outlook.com>
2023-08-09 10:05:36 +0000

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+ARCH='amd64'
+
+NAME=nat-type
+
+for os in linux darwin windows; do
+    name="$NAME-$os-$ARCH"
+    if [ "$os" == "windows" ]; then
+        name="$name.exe"
+    fi
+    GOOS=$os GOARCH=$ARCH go build -o "$name"
+    sha256sum "$name" >"$name.sha256"
+done
